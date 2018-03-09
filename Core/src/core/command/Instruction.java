26f5e0bd482872fc75fa33e6e93885248fe5dd15
s-afzalr <s-afzalr@bsd405.org>
2018-03-08 18:15:38 -0800

    Fixed Build Path problems

--- Core/src/core/command/Instruction.java
+++
@@ -1,9 +0,0 @@
-package core.command;
-
-import java.io.Serializable;
-
-import client.Client;
-
-public interface Instruction extends Serializable {
-	public void run(Client client, String[] args);
-}
